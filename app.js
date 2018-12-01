9e31e12687a7c5454227c86c036a6bfed54eef38
marivech <maria.vechkanova@gmail.com>
2018-12-01 14:32:27 +0300

    basic game features

--- app.js
+++ app.js
@@ -8,3 +8,72 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, currentPlayer, roundScore;
+
+scores = [0, 0];
+currentPlayer = 0;
+roundScore = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  var dice = Math.floor(Math.random() * 6 + 1);
+  var diceDom = document.querySelector('.dice');
+
+  diceDom.src = 'img/dice-' + dice + '.png';
+  diceDom.style.display = 'block';
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + currentPlayer).textContent = roundScore;
+  } else {
+    changePlayer();
+  }
+});
+
+function changePlayer() {
+  currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  scores[currentPlayer] += roundScore;
+  document.getElementById('score-' + currentPlayer).textContent = scores[currentPlayer];
+
+  if (scores[currentPlayer] >= 10) {
+    document.getElementById('name-' + currentPlayer).textContent = 'Winner';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
+  } else {
+    changePlayer();
+  }
+});
+
+document.querySelector('.btn-new').addEventListener('click', function() {
+  currentPlayer = 0;
+  changePlayer();
+  scores = [0, 0];
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+});
