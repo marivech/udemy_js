775e324d51bb3b1dbe8de7fec91f768e71c27a83
marivech <maria.vechkanova@gmail.com>
2019-03-27 00:48:18 +0300

    es7challenge reviewed

--- es7challenge/script.js
+++ es7challenge/script.js
@@ -35,61 +35,79 @@ class Obj {
 class Park extends Obj {
   constructor(name, buildYear, treesNum, area) {
     super(name, buildYear)
-    this.area = area;
+    this.area = area; // km2
     this.treesNum = treesNum;
+  }
 
-    this.density = this.treesNum / this.area;
+  calculateTreeDensity() {
+    return this.treesNum / this.area;
   }
 };
 
 class Street extends Obj {
-  constructor(name, buildYear, length, size = 'normal') {
+  constructor(name, buildYear, length, size = 3) {
     super(name, buildYear)
 
     this.length = length;
     this.size = size;
   }
-};
-// no actual need in maps, but used to practise
-const parkMap = new Map();
-parkMap.set('istanbul', new Park('Стамбул', 2010, 40, 100));
-parkMap.set('les', new Park('Г-О Лес', 2005, 1200, 356));
-parkMap.set('kaban', new Park('озеро Кабан', 2018, 200, 345));
 
-const streetMap = new Map();
-streetMap.set('bondarenko', new Street('Бондаренко', 1980, 1.3, 'small'));
-streetMap.set('chistay', new Street('Чистопольская', 1963, 4.1, 'big'));
-streetMap.set('yamasheva', new Street('Ямашева', 1968, 3.9));
-streetMap.set('test', new Street('test', 1968, 2.4, 'huge'));
+  classifyStreet() {
+    const classification = new Map();
+    classification.set(1, 'tiny');
+    classification.set(2, 'small');
+    classification.set(3, 'normal');
+    classification.set(4, 'big');
+    classification.set(5, 'huge');
 
-// task 1
-for (const [key, value] of parkMap) {
-  console.log(`Tree density of ${value.name} is ${value.density}.`);
-}
-
-// task 2
-let ageSum = 0;
-for (const [key, value] of parkMap) {
-  ageSum =+ value.age
-}
-console.log(`Average age of parks is ${ageSum / parkMap.size}.`);
-
-// task 3
-for (const [key, value] of parkMap) {
-  if (value.treesNum > 1000) {
-    console.log(`${value.name} has more than 1000 trees.`);
+    return classification.get(this.size)
   }
+};
+
+const parks = [
+  new Park('Park 1', 2010, 40, 100),
+  new Park('Park 2', 2005, 1200, 356),
+  new Park('Park 3', 2018, 200, 345)
+];
+
+const streets = [
+  new Street('Street 1', 1980, 1.3, 2),
+  new Street('Street 2', 1963, 4.1, 4),
+  new Street('Street 3', 1968, 3.9),
+  new Street('test', 1968, 2.4, 5),
+];
+
+function parkReport(p) {
+  console.log('--- Park report ---');
+
+  // task 1
+  p.forEach(element => {
+    console.log(`Tree density of ${element.name} is ${element.calculateTreeDensity()} trees per square km.`);
+  });
+
+  // task 2
+  const ageSum = p.reduce((sum, elm) => sum + elm.age, 0);
+  console.log(`Average age of parks is ${ageSum / p.length}.`);
+
+  // task 3
+  const treedParkIndex = p.findIndex(elm => elm.treesNum > 1000);
+  console.log(`${p[treedParkIndex].name} has more than 1000 trees`);
 }
 
-// task 4
-let streetLength = 0;
-for (const [key, value] of streetMap) {
-  streetLength += value.length;
-}
-console.log(`Total length of streets is ${streetLength}`);
-console.log(`Average length of streets is ${streetLength / streetMap.size}`);
+function streetReport(s) {
+  console.log('--- Street report ---');
 
-// task 5 
-for (const [key, value] of streetMap) {
-  console.log(`${value.name} is ${value.size}`);
+  // task 4
+  const lengthSum = s.reduce((sum, elm) => sum + elm.length, 0);
+  console.log(`Total length of streets is ${lengthSum}`);
+  console.log(`Average length of streets is ${lengthSum / s.length}`);
+
+  // task 5
+  s.forEach(elm => {
+    console.log(`${elm.name} is ${elm.classifyStreet(this.size)}`);
+  });
 }
+
+parkReport(parks);
+streetReport(streets);
+
